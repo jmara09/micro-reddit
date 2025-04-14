57b05a0c8dcf09cb3f8bc3ecd96d2d2a3b4a7cf0
Jonnel Marasigan <jmara.0923@gmail.com>
2025-04-14 20:50:42 +0300

    Fix bug not associating user to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, uniqueness: true, length: { in: 4..12 }, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, length: { in: 6..16 }, presence: true
