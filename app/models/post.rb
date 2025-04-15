c1611a94083a3dbd8db950500bf190e287b91207
Jonnel Marasigan <jmara.0923@gmail.com>
2025-04-15 06:27:51 +0300

    Fix post not recognizing comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :user, presence: true
   validates :title, uniqueness: true, presence: true
   validates :body, presence: true
